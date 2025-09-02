3690d342ba5540511c1761efb4217016e1250b8d
BirajT <birajtamrakar803@gmail.com>
2025-09-02 08:51:35 +0545

    branch commit

--- javascript/BEAST_JS/Day-11.js
+++ javascript/BEAST_JS/Day-11.js
@@ -42,5 +42,8 @@
 // git checkout <branch_name> ->switch branch
 // git checkout -b <new_branch_name> => create new branch +switch
 // git branch -d <branch_name> => delete branch locally
+// git pull <branch_name>
+//git merge <branch-name>
+
 
 
