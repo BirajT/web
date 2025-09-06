2153ce499ee532d6fb55d9d18acf6112d7a4cadf
BirajT <birajtamrakar803@gmail.com>
2025-09-06 10:35:03 +0545

    OOP and Inheritance

--- javascript/BEAST_JS/Day-15HW.js
+++ javascript/BEAST_JS/Day-15HW.js
@@ -29,21 +29,25 @@ class Teacher{
 const t1=new Teacher(6571,"Biraj","Kathmandu",200000,"Computer");
 t1.display()
 class Student extends Teacher{
-    id;
-    name;
-    address;
     faculty;
+    fees;
 
-    constructor(id,name,address,faculty)
+    constructor(id,name,address,fees,subject,faculty)
     {
-        super(id,name,address)
+        super(id,name,address,0,subject)
+        this.fees=fees;
         this.faculty=faculty
     }
-    display()
-    {
-        super.display()
-    }
-
+  displays()
+  {
+     console.log("Student Info:");
+        console.log("ID:", this.id);
+        console.log("Name:", this.name);
+        console.log("Address:", this.address);
+        console.log("Faculty:", this.faculty);
+        console.log("Fees:", this.fees);
+        console.log("Subject:", this.subject);
+  }
 }
-const S1=new Student(6677,"sandesh","Butwal",180000,"BIT") 
-S1.display()
+const S1=new Student(6677,"sandesh","Butwal","800000","BIT","Sci and Tech") 
+S1.displays()
