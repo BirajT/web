84bc2240d8532e77defc82d3f24ec0297e35e5e2
BirajT <birajtamrakar803@gmail.com>
2025-09-08 22:29:28 +0545

    Error handling try catch finally

--- javascript/BEAST_JS/Day-17.js
+++ javascript/BEAST_JS/Day-17.js
@@ -1,1 +1,30 @@
 //hell call back
+// promise is an object {} which repred
+
+console.log("Start");
+const promise=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        //task
+        //resolve('task completed')
+        reject("Something went wrong");
+    },2000);
+});
+
+console.log(promise);
+
+promise
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+    console.log("end");
+
+    //
+    console.log("Start");
+
+    
+        
+    
+    
