d039a5dc19a8c2c3ebf85f9187b270e58c207936
BirajT <birajtamrakar803@gmail.com>
2025-09-10 23:56:48 +0545

    pp

--- javascript/BEAST_JS/Day-19.js
+++ javascript/BEAST_JS/Day-19.js
@@ -1,5 +1,14 @@
 //Combine question practice
-class Calculator {
-    constructor(a, b) {
-        this.a = a;
-        this.b
+// Write a JavaScript program using class, object, function, try-catch-finally with the following requirements:
+
+// Create a class BankAccount with properties name and balance.
+
+// Add a function withdraw(amount) inside the class that:
+
+// Throws an error if the amount is greater than the current balance.
+
+// Otherwise, deducts the amount from balance and returns the updated balance.
+
+// Use try-catch-finally inside the withdraw function to handle errors.
+
+// Create an object of BankAccount and perform both a valid and invalid withdrawal.
