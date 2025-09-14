093c8dfdc26c0c12d523754e2de584219619cce5
BirajT <birajtamrakar803@gmail.com>
2025-09-14 23:23:24 +0545

    largest,odd_sum,even

--- javascript/BEAST_JS/Day-22.js
+++ javascript/BEAST_JS/Day-22.js
@@ -7,24 +7,43 @@
 // Even numbers: 2, 8, 10
 // Sum of odd numbers: 15
 // Largest number: 10
-a=[];
-b=[];
+let a=[];
 function input()
 {
+    let sum=0;
+    let l=0;
+   let c=0;
+   let b;
     for(i=0;i<6;i++)
     {
         const prompt = require("prompt-sync")();
         let num=Number(prompt("Enter a number: "));
         a.push(num);
         console.log(a[i]);
-        b=a.filter(function(value,index,arr)
+       
+       
+       
+    }
+     c=a.filter(function(value,index,arr)
+        {   
+            return value%2==1;
+        })
+      b=a.filter(function(value,index,arr)
         {
             return value%2==0;
         })
-        
+        for(i=0;i<b.length;i++)
+        {
+         sum+=c[i]
+        if(b[i]<b[i+1])
+        {
+            l=b[i+1];
+        }
     }
     
     console.log(b);
+    console.log("sum = ",sum);
+    console.log("Largest = ",l);
     
 }
 input()
