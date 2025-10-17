5de006e0040b46e1938337f383c45dc5b8e5e657
BirajT <birajtamrakar803@gmail.com>
2025-10-17 10:09:51 +0545

    server created of method get post put delete

--- javascript/Express/index.js
+++ javascript/Express/index.js
@@ -21,7 +21,7 @@ app.get('/about',(req,res)=>{
     res.send('<h1>About_page')
 })
 
-app.get('/users/all',(req,res)=>{
+app.get('/users',(req,res)=>{
     res.json({
         data:[
             {id:1,name:"abc",email:"abc@gmail.com"},
@@ -32,7 +32,7 @@ app.get('/users/all',(req,res)=>{
 });
 
 //dynamic route {req,params}
-app.get ('/users/:id/:name',(req,res)=>{
+app.get ('/users/:id',(req,res)=>{
     console.log(req.params);
     const params=req.params
     
@@ -40,6 +40,62 @@ app.get ('/users/:id/:name',(req,res)=>{
         data:{id:params.id,name:params.name,email:"abc@gmail.com"}
     })
     })
+
+app.post ('/users',(req,res)=>{
+  res.status(201).json({
+    message:'Account created'
+  })
+})
+     
+
+  app.put('/users/:id',(req,res)=>{
+    const{id}=req.params
+    res.status(200).json({
+        message:`Profile updated for user id ${id}`
+    })
+  })
+
+  app.delete('/users/:id',(req,res)=>{
+    const{id}=req.params
+    res.status(200).json({
+        message:`profile deleted for user id ${id}`
+    })
+  })
+    
+ //product
+ app.get ('/products/:id',(req,res)=>{
+    console.log(req.params);
+    const params=req.params
+    
+    res.json({
+        data:{id:params.id,name:params.name,}
+    })
+    })
+
+app.post ('/products',(req,res)=>{
+  res.status(201).json({
+    message:'Product added'
+  })
+})
+     
+
+  app.put('/products/:id',(req,res)=>{
+    const{id}=req.params
+    res.status(200).json({
+        message:`Profile updated for product id ${id}`
+    })
+  })
+
+  app.delete('/products/:id',(req,res)=>{
+    const{id}=req.params
+    res.status(200).json({
+        message:`product deleted of id ${id}`
+    })
+  })
+
+     
+
+
 
 //listening
 server.listen(PORT,()=>{
