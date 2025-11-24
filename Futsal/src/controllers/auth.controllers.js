9f156e35f7018c52f5b1a5259dcbcbd7067bf3ff
BirajT <birajtamrakar803@gmail.com>
2025-11-24 15:29:06 +0545

    futsal error handler

--- Futsal/src/controllers/auth.controllers.js
+++ Futsal/src/controllers/auth.controllers.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose"
+import { asynchandler } from "../utils/asynchandler.utils"
+import CustomError from "../middleware/error_handler"
 
-export const register=async(req,res)=>{
+export const register=asynchandler(async(req,res)=>{
     const{name,email,password}=req.body
+    const image=req.file
+    if(!name)
+    {
+        throw new CustomError("name is required",400)
+    }
+    if(!email)
+    {
 
-    if(!name || !password || !email){
-        res.status(400).json({
-            message:"password required",
-            success:false
-        })
     }
-    try {
-        
-    } catch (error) {
-        res.json({success:false,message:error,message})
-    }
-}
+
+})
