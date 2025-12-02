bac1db9599c56e4e57fb2662e4d1f1a43a41549e
BirajT <birajtamrakar803@gmail.com>
2025-12-02 14:01:40 +0545

    booking model completed

---
+++ Futsal/src/models/booking.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import { BOOKING_STATUS, PAYMENT_STATUS } from "../constants/enums.constants";
+
+const bookingSchema=new mongoose.Schema({
+    futsal:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Futsal",
+        required:true
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    date:{
+        type:String,
+        required:true
+    },
+    start_time:{
+        type:String,
+        required:true,
+    },
+    end_time:{
+        type:String,
+        required:true,
+    },
+    total_price:{
+        type:Number,
+        required:true
+    },
+    payment_status:{
+        type:String,
+        enum:Object.values(PAYMENT_STATUS),
+        default:PAYMENT_STATUS.PENDING
+    },
+    booking_status:{
+        type:String,
+        enum:Object.values(BOOKING_STATUS),
+        default:BOOKING_STATUS.PENDING
+    }
+
+
+
+},{timestamps:true})
+
+export default mongoose.model("booking",bookingSchema)
