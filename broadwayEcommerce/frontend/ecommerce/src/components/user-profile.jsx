9abdc8735b35c8e0559850e8323f7aec76450faa
BirajT <birajtamrakar803@gmail.com>
2025-12-09 16:03:12 +0545

    updated user profile.jsx

--- broadwayEcommerce/frontend/ecommerce/src/components/user-profile.jsx
+++ broadwayEcommerce/frontend/ecommerce/src/components/user-profile.jsx
@@ -1,24 +1,45 @@
-export const UserProfile=({user})=>{
-    const a=10;
-    return (
-        <>
-        <div>
-        <p>name:{user.name}</p>
-        <p>email:{user.email}</p>
-        <p>phoneno:{user.phoneno || '-----'}</p>
-        <p>count:{a}</p>
-        </div>
-        </>
+import { useState } from "react";
 
-    );
+export const UserProfile = () => {
+
+  // const input = ''
+  const [value , setValue] = useState('')
+  // const {name , email} = props
+  console.log('user profile changes')
+
+  const onEdit = () => {
+    alert(`input value ${value}`)
+
+    console.log(`input value ${value}`)
+  }
+
+  const onChange = (e) => {
+    console.log(e.target.value)
+    setValue(e.target.value)
+
+  }
+
+  return (
+    <>
+      <div>
+        <p>name:{ '-'}</p>
+        <p>email:{ '-'  }</p>
+        <p>phone:{'-'}</p>
+        <input type="text" onChange={onChange} />
+        <input onChange={(e)=>{console.log(e.target.value)}} type="number"/>
+        <input type="file"/>
+        <input type="radio" />
+        <input type="submit" />
+        <input type="range" />
+        <input type="checkbox" />
+        <input onChange={(e)=>{console.log(e.target.value)}} type="color" />
+        <input type="date" />
+        <input type="time" />
+        <input type="datetime-local" />
+        <input type="week" />
+        
+        <button onClick={onEdit}>Edit</button>
+      </div>
+    </>
+  );
 };
-
-export const Multiply=({mul})=>{
-    return(
-        <div>
-            <p>MULTIPLY</p>
-            <p>{mul.no}*{mul.n}={mul.soln}</p>
-        </div>
-    )
-}
-
