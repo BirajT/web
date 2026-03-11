83943e843f8ebe02dd0f71d0435ca934c5999e61
BirajT <birajtamrakar803@gmail.com>
2026-03-11 20:15:32 +0545

    menu model completed

--- Canteen/src/models/menu.model.js
+++ Canteen/src/models/menu.model.js
@@ -1,8 +1,35 @@
 import mongoose from 'mongoose'
+import { AVAILABILITY } from '../constants/enums.constants'
 
 const menuSchema=new mongoose.Schema({
-    food_name:{
+    name:{
         type:String,
-        required:{true,"name is required"}
+        required:[true,"name is required"]
+    },
+    _id:{
+        type:string,
+        required:[true,"id is required"]
+    },
+    price:{
+        type:string,
+        required:[true,"price is required"]
+    },
+    category:{
+        type:string,
+        required:[true,"category is required"]
+    },
+    image:{
+        type: {
+            path: String,
+            public_id:String
+        }
+    },
+    availability:{
+        type:String,
+        enum:Object.values(AVAILABILITY)
     }
-})
+},{timestamps:true})
+
+const Menu=mongoose.model('menu',menuSchema)
+
+export default Menu
