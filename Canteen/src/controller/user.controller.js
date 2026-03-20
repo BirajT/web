1677e709c879d053e319609cfb6095b187b06046
BirajT <birajtamrakar803@gmail.com>
2026-03-20 13:57:56 +0545

    user controller

--- Canteen/src/controller/user.controller.js
+++ Canteen/src/controller/user.controller.js
@@ -1,5 +1,22 @@
+import CustomError from "../middleware/error_handler.middleware";
 import { asyncHandler } from "../utils/asynchandler.utils";
 
-export const getAll=asyncHandler(async(req,resizeBy,next)=>{
-    
+export const getAll=asyncHandler(async(req,res,next)=>{
+    const users=await UserActivation.find({});
+    res.status(200).json({
+        message:"user fetched",
+        data:users,
+    });
 })
+export const getById=asyncHandler(async(req,res,next)=>{
+    const {id}=req.params
+    const user=await User.findById(id)
+
+    if(!user){
+        throw new CustomError('user not found',404)
+    }
+
+    res.status(200).json({
+        message:'user by id',
+        data:user
+    })
