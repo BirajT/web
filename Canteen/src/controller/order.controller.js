ffa149fe016bfdc30862ced2db02256c41ffd0b6
BirajT <birajtamrakar803@gmail.com>
2026-04-05 23:05:21 +0545

    order controller

--- Canteen/src/controller/order.controller.js
+++ Canteen/src/controller/order.controller.js
@@ -1,5 +1,36 @@
-import { asyncHandler } from "../utils/asynchandler.utils";
+import Order from "../models/order.model.js";
+import Menu from "../models/menu.model.js";
 
-export const order=asyncHandler(async(req,res,next)=>{
-    
-})
+export const createOrder = async (req, res) => {
+  try {
+    const { items } = req.body; // [{ itemId, quantity }]
+
+    let totalAmount = 0;
+
+    // calculate total price
+    for (const item of items) {
+      const menuItem = await Menu.findById(item.itemId);
+
+      if (!menuItem) {
+        return res.status(404).json({ message: "Item not found" });
+      }
+
+      totalAmount += menuItem.price * item.quantity;
+    }
+
+    const order = await Order.create({
+      userId: req.user._id,
+      items,
+      totalAmount,
+      status: "pending",
+      orderTime: new Date()
+    });
+
+    res.status(201).json({
+      success: true,
+      data: order
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
