4f17f44cdeb4a6f020eead85b0f00572c2d52339
BirajT <birajtamrakar803@gmail.com>
2026-04-11 00:23:41 +0545

    order model update

--- Canteen/src/models/order.model.js
+++ Canteen/src/models/order.model.js
@@ -1,15 +1,28 @@
 import mongoose from "mongoose"
-
+import { STATUS } from "../constants/enums.constants.js"
 const orderSchema=new mongoose.Schema({
     menu:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"menu",
         required:true
-    }
-    ,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
     quantity:{
-        type:String,
+        type:Number,
         required:[true,"quantity is required"]
+    },
+    status: {
+        type: String,
+        enum:Object.values(STATUS),
+        default: STATUS.PENDING
+    },
+    totalPrice: {
+        type: Number,
+        required: false
     }
 },{timestamps:true})
 
