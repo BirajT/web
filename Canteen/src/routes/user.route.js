2b9dd17b106b96a1c9e7ebbd8f3093369949ec33
BirajT <birajtamrakar803@gmail.com>
2026-04-13 00:55:37 +0545

    user routes

--- Canteen/src/routes/user.route.js
+++ Canteen/src/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { getAll, getById, remove } from "../controller/user.controller.js"
+import authenticate from "../middleware/authenticate.middleware.js"
+import { USER_ROLE } from "../constants/enums.constants.js"
+
 const router=express.Router()
 
-router.get("/getAll",getAll)
-router.get("/:id",getById)
-// router.put('/:id',updateUser)
-router.delete('/:id',remove)
+router.get("/getAll", authenticate([USER_ROLE.ADMIN, USER_ROLE.OWNER]), getAll)
+router.get("/:id", authenticate(), getById)
+router.delete('/:id', authenticate([USER_ROLE.ADMIN]), remove)
 
 export default router
 
