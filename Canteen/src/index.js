88e9c5a3ec1200463616f7a8376e0851ab83353e
BirajT <birajtamrakar803@gmail.com>
2026-04-16 16:32:34 +0545

    order route and category api integrated

--- Canteen/src/index.js
+++ Canteen/src/index.js
@@ -1,27 +1,38 @@
 import 'dotenv/config'
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 import { connectDB } from './config/db.config.js'
 import { errorHandler } from "./middleware/error_handler.middleware.js"
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from "./routes/user.route.js"
 import menuRoutes from "./routes/menu.route.js"
-const PORT=process.env.PORT
-const app=express()
+import orderRoutes from "./routes/order.routes.js"
+import categoryRoutes from "./routes/category.routes.js"
+
+const PORT = process.env.PORT
+const app = express()
 
 connectDB()
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
+app.use(cookieParser())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/auth',authRoutes)
 app.use('/api/user',userRoutes)
 app.use('/api/menu',menuRoutes)
-app.use('/api/order')
+app.use('/api/order',orderRoutes)
+app.use('/api/category',categoryRoutes)
 
 
 app.get('/',(req,res)=>{
     res.status(200).json({
-        messsage:"server is up and running"
+        message:"server is up and running"
     });
 });
 app.use(errorHandler)
