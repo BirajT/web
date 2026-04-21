bc3e9a119bc3be8211648e2cc713b7d8c9dbd42c
BirajT <birajtamrakar803@gmail.com>
2026-04-21 14:47:38 +0545

    CRM database setup progress

---
+++ CRM/src/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import 'dotenv/config'
+import { connectDB } from "./config/db.config.js";
+
+const PORT=process.env.PORT
+const app=express();
+
+connectDB()
+
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:`server is up and running `
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Database is connected at http://localhost:${PORT}`)
+})
